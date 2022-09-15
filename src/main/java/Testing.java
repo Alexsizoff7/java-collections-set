a9802c1339191deb7feff2cf7f496f459ec23a17
Alexsizoff7 <alexei.sizov@endava.com>
2022-09-15 16:45:45 +0300

    implemented hashSet

---
+++ src/main/java/Testing.java
@@ -0,0 +1,67 @@
+import com.endava.internship.collections.Student;
+import com.endava.internship.collections.StudentSet;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class Testing {
+    public static void main(String[] args) {
+//        List<Student> st = List.of(new Student("name", LocalDate.now(), "details1" ),
+//                new Student("name2", LocalDate.now(), "details2" ));
+//
+//        st.toArray(new Student[0]);
+
+
+        StudentSet test = new StudentSet(5);
+        Student st1 = new Student("Mark",LocalDate.of(1990,01,20),"Employee");
+        Student st2 = new Student("Alex",LocalDate.of(1995,03,18),"Intern");
+        Student st3 = new Student("John",LocalDate.of(1993,04,22),"Intern");
+        Student st4 = new Student("Marie",LocalDate.of(1996,07,4),"Intern");
+        Student st5 = new Student("James",LocalDate.of(1992,11,12),"Mentor");
+        Student st6 = new Student("Rob",LocalDate.of(1991,02,6),"Line Manager");
+        Student st7 = new Student("Brad",LocalDate.of(1999,05,28),"Intern");
+        StudentSet test2 = new StudentSet();
+
+
+
+//        System.out.println(test.size());
+//        System.out.println(test.add(st1));
+//        System.out.println(test.add(st3));
+//        System.out.println(test.add(st3));
+//        System.out.println(test.add(st5));
+//        System.out.println(test.size());
+//        System.out.println(test.remove(st1));
+//        System.out.println(test.size());
+//        System.out.println(test.remove(st1));
+//        System.out.println(test.size());
+        test.add(st1);
+        test.add(st2);
+        test.add(st3);
+        test.add(st4);
+        System.out.println(test);
+        test.add(st5);
+        System.out.println(test);
+        test.add(st6);
+        test.add(st7);
+        test.add(st1);
+        System.out.println(test);
+
+        System.out.println(test.size());
+//        System.out.println(Arrays.toString(test.toArray(new ABC[0])));
+
+//        Set<Student> sts = Set.of(st1, st2, st3);
+//        sts.toArray(new ABC[0]);
+
+        System.out.println("\nIterator starts from here");
+        Iterator <Student> itr = test.iterator();
+
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+
+    }
+}
+
+class ABC {
+
+}
--- src/main/java/com/endava/internship/collections/StudentSet.java
+++ src/main/java/com/endava/internship/collections/StudentSet.java
@@ -1,68 +1,209 @@
 package com.endava.internship.collections;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 public class StudentSet implements Set<Student> {
+
+    private int capacity;
+    private List <LinkedList<Student>> buckets;
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 16;
+
+    public StudentSet (int capacity) {
+        this.capacity = capacity;
+        buckets = new ArrayList <>(capacity);
+        for(int i=0; i < capacity;i++) {
+            buckets.add(new LinkedList<>());
+        } 
+    }
+
+    public StudentSet () {
+        this(DEFAULT_CAPACITY);
+    }
+
     @Override
     public int size() {
-        //TODO
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        //TODO
-        return false;
+        return size == 0;
     }
 
     @Override
     public boolean contains(Object o) {
-        //TODO
+        if (o instanceof Student) {
+            Student student = (Student) o;
+            int index = calculatedIndex(student);
+            LinkedList <Student> bucket = buckets.get(index);
+            for (Student st : bucket) {
+                if (student.hashCode() == st.hashCode() && student.equals(st)) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public Iterator<Student> iterator() {
-        //TODO
-        return null;
+        return new StudentIterator();
     }
 
     @Override
     public Object[] toArray() {
-        //TODO
-        return new Object[0];
+        Object [] array = new Object[size];
+        int counter = 0;
+        for (List <Student> bucket : buckets) {
+            for (Student student : bucket) {
+                array[counter] = student;
+                counter++;
+            }
+        }
+        return array;
     }
 
     @Override
     public <T> T[] toArray(T[] ts) {
-        //TODO
-        return null;
+        if (ts.length < size) {
+            ts = (T[]) java.lang.reflect.Array
+                    .newInstance(ts.getClass().getComponentType(), size);
+        }
+
+        int counter = 0;
+        for (List <Student> bucket : buckets) {
+            for (Student student : bucket) {
+                ts[counter] = (T) student;
+                counter++;
+            }
+        }
+            for (int i = counter; counter<ts.length-1; i++) {
+                ts[i] = null;
+            }
+
+        // counter < ts.len-1 then ts[counter+1, ..., len-1] = null
+        return ts;
     }
 
     @Override
     public boolean add(Student student) {
-        //TODO
-        return false;
+        resizeCheck();
+        int index = calculatedIndex(student);
+        LinkedList <Student> bucket = buckets.get(index);
+        for (Student st : bucket) {
+            if (student.hashCode() == st.hashCode() && student.equals(st)) {
+                return false;
+            }
+        }
+        bucket.add(student);
+        size++;
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        //TODO
+        if (o instanceof Student) {
+            Student student = (Student) o;
+            int index = calculatedIndex(student);
+            LinkedList <Student> bucket = buckets.get(index);
+            for (Student st : bucket) {
+                if (student.hashCode() == st.hashCode() && student.equals(st)) {
+                    bucket.remove(student);
+                    size--;
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public void clear() {
-        //TODO
+
+        List <LinkedList<Student>> clearedBuckets;
+        clearedBuckets = new ArrayList <>(capacity);
+        for(int i=0; i < capacity; i++) {
+            clearedBuckets.add(new LinkedList<>());
+        }
+        buckets = clearedBuckets;
+        size = 0;
     }
 
     @Override
     public boolean addAll(Collection<? extends Student> collection) {
-        //TODO
-        return false;
+        boolean modified = false;
+        for (Student student : collection) {
+            if (this.add(student)) {
+                modified = true;
+            }
+        }
+        return modified;
     }
+
+    private int calculatedIndex (Student student) {
+        int hashCode = student.hashCode();
+        return Math.abs(hashCode % capacity);
+    }
+
+    void resizeCheck () {
+        if (size>(capacity*0.75)) {
+            capacity *= 2;
+            size = 0;
+            List<LinkedList<Student>> oldBuckets = buckets;
+            buckets = new ArrayList<>(capacity);
+            for (int i = 0; i < capacity; i++) {
+                buckets.add(new LinkedList<>());
+            }
+            for (LinkedList<Student> oldBucket : oldBuckets) {
+                this.addAll(oldBucket);
+            }
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "StudentSet{" +
+                "capacity=" + capacity +
+                ", buckets=" + buckets +
+                ", size=" + size +
+                '}';
+    }
+
+    private class StudentIterator implements Iterator <Student> {
+
+        // init cursor 1,2 -> curr bucket, current index
+        // toArray() and store its result
+
+        private int current;
+        private int size;
+        Object [] elements;
+
+        public StudentIterator () {
+            size = size();
+            elements = toArray();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current != size;
+        }
+
+        @Override
+        public Student next() {
+            int elementIndex = current;
+            if (elementIndex >= size) {
+                throw new NoSuchElementException();
+            }
+            Student nextStudent = null;
+
+            nextStudent = (Student) elements[elementIndex];
+            current++;
+            return nextStudent;
+        }
+    }
+
 
     @Override
     public boolean containsAll(Collection<?> collection) {
@@ -82,3 +223,5 @@ public class StudentSet implements Set<Student> {
         throw new UnsupportedOperationException();
     }
 }
+
+
